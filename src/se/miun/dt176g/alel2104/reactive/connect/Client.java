8e2a57a29e7b3c716d0469c5fa60d91384645272
alel2104 <alel2104@student.miun.se>
2023-10-02 15:36:12 +0200

    Started adding Client logic for sending and receiving shapes.

--- src/se/miun/dt176g/alel2104/reactive/connect/Client.java
+++ src/se/miun/dt176g/alel2104/reactive/connect/Client.java
@@ -1,4 +1,78 @@
 package se.miun.dt176g.alel2104.reactive.connect;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import se.miun.dt176g.alel2104.reactive.Shape;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+    private final Socket socket;
+    private final CompositeDisposable disposables;
+
+    public Client(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        disposables = new CompositeDisposable();
+    }
+
+    public void startClient() {
+        Disposable getShapeDisposable = getShapeObservable()
+                .observeOn(Schedulers.io())
+                .doOnDispose(socket::close)
+                .subscribe(shape -> System.out.println(shape.getClass()), System.err::println);
+
+        disposables.add(getShapeDisposable);
+    }
+
+    public void stopClient() {
+        disposables.clear();
+        Schedulers.shutdown();
+    }
+
+    public Observable<Shape> getShapeObservable() {
+        return Observable.defer(() -> {
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                return Observable.just((Shape) objectInputStream.readObject());
+            } catch (IOException | ClassNotFoundException e) {
+                return Observable.error(new RuntimeException(e));
+            }
+        }).subscribeOn(Schedulers.io());
+
+//        return Observable.just(socket)
+//                .subscribeOn(Schedulers.io())
+//                .map(Socket::getInputStream)
+//                .map(ObjectInputStream::new)
+//                .map(objectInputStream -> (Shape) objectInputStream.readObject());
+    }
+
+    public void sendShape(Shape shape) {
+        Disposable sendShapeDisposable = Observable.defer(() -> {
+            try {
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                outputStream.writeObject(shape);
+                return Observable.just(shape);
+            } catch (IOException e) {
+                return Observable.error(new RuntimeException(e));
+            }
+                }).subscribeOn(Schedulers.io())
+                .subscribe(shape1 -> System.out.println(shape1.getClass()), System.err::println);
+
+            disposables.add(sendShapeDisposable);
+//        Observable.just(socket)
+//                .subscribeOn(Schedulers.io())
+//                .map(Socket::getOutputStream)
+//                .map(ObjectOutputStream::new)
+//                .map(objectOutputStream -> {
+//                    objectOutputStream.writeObject(shape);
+//                    return shape;
+//                })
+//                .subscribe(shape1 -> System.out.println(shape1.getClass()), System.err::println);
+    }
+
 }
